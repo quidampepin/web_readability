224efa54107866f04af68f838779f6504b1fe436
quidampepin <pepin.david@gmail.com>
2020-05-11 09:03:28 -0400

    Tweaking code for clarity

--- web_read.py
+++ web_read.py
@@ -8,37 +8,37 @@ html = r.text
 
 #get the html content as text - get content from the "main" tag
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(html, features="lxml").find('main')
-text1 = soup.get_text()
+original_soup = BeautifulSoup(html, features="lxml").find('main')
+original_text = original_soup.get_text()
 
 #get initial readability total_score
 from readability import Readability
-r = Readability(text1)
-fk1 = r.flesch_kincaid()
+r_o = Readability(original_text)
+original_fk = r_o.flesch_kincaid()
 
 #add periods after bullet points and headings so that the Flesch Kicaid score considers them as sentences
-html = html.replace("</li>", ".</li>")
-html = html.replace("<h1>", ".</h1>")
-html = html.replace("<h2>", ".</h2>")
-html = html.replace("<h3>", ".</h3>")
-html = html.replace("<h4>", ".</h4>")
-html = html.replace("<h5>", ".</h5>")
-html = html.replace("<h6>", ".</h6>")
+html1 = html.replace("</li>", ".</li>")
+html2 = html1.replace("</h1>", ".</h1>")
+html3 = html2.replace("</h2>", ".</h2>")
+html4 = html3.replace("</h3>", ".</h3>")
+html5 = html4.replace("</h4>", ".</h4>")
+html6 = html5.replace("</h5>", ".</h5>")
+html7 = html6.replace("</h6>", ".</h6>")
 
 #get adjusted readability total_score
-soup = BeautifulSoup(html, features="lxml").find('main')
-text = soup.get_text()
+revised_soup = BeautifulSoup(html7, features="lxml").find('main')
+revised_text = revised_soup.get_text()
 
 from readability import Readability
-r = Readability(text)
-fk = r.flesch_kincaid()
+r_f = Readability(revised_text)
+final_fk = r_f.flesch_kincaid()
 
 
 #tokenize the text for processing
 import nltk
 from nltk.tokenize import RegexpTokenizer
 tokenizer = RegexpTokenizer('\w+')
-tokens = tokenizer.tokenize(text)
+tokens = tokenizer.tokenize(revised_text)
 words = []
 for word in tokens:
     words.append(word.lower())
@@ -56,26 +56,21 @@ from nltk import FreqDist
 fdist1 = FreqDist(words_ns)
 most_common = fdist1.most_common(15)
 
-#get readability score from the text
-from readability import Readability
-r = Readability(text)
-fk = r.flesch_kincaid()
-
 #get all headings and calculate how many words on average between headings
-headings = BeautifulSoup(html, features="lxml").findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
-hratio = (len(words)/len(headings))
+headings = original_soup.findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+hratio = len(words)/(len(headings))
 
 #get all paragraphs and all bulleted list, and calculate how many words per paragraph on average
-paragraphs = BeautifulSoup(html, features="lxml").findAll(['p', 'ul'])
+paragraphs = original_soup.findAll(['p', 'ul'])
 pratio = (len(words)/len(paragraphs))
 
 #calculate points for readability
-if fk.score <= 6:
+if final_fk.score <= 6:
     fkpoints = 60
-elif fk.score >= 18:
+elif final_fk.score >= 18:
     fkpoints = 0
 else :
-    fkpoints = (60-((fk.score-6)*5))
+    fkpoints = (60-((final_fk.score-6)*5))
 
 #calculate points for number of words between headings
 if hratio <= 40:
@@ -97,9 +92,9 @@ else :
 total_score = fkpoints+hpoints+ppoints
 
 #print results
-print ('Initial readability score:', format(fk1.score, '.2f'))
+print ('Initial readability score:', format(original_fk.score, '.2f'))
 print ('Total points:', format(total_score, '.2f'), '/100')
-print ('Basic readabilty points:', format(fkpoints, '.2f'), '/60', '(Flesch-Kincaid readability score of:', format(fk.score, '.2f'), ')')
+print ('Basic readabilty points:', format(fkpoints, '.2f'), '/60', '(Flesch-Kincaid readability score of:', format(final_fk.score, '.2f'), ')')
 print ('Heading points: ', format(hpoints, '.2f'), '/20', '(words per heading:', format(hratio, '.2f'), ')')
 print ('Paragraph points: ', format(ppoints, '.2f'), '/20', '(words per paragraph:', format(pratio, '.2f'), ')')
 print('Number of words: ', len(words))
