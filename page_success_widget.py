f049ca38dd0ae2601d73268e6abdf1c4eb376085
quidampepin <pepin.david@gmail.com>
2020-05-22 08:10:29 -0400

    Update page_success_widget.py

--- page_success_widget.py
+++ page_success_widget.py
@@ -3,6 +3,7 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 import nltk
+from wordcloud import WordCloud
 
 #import CSV file as a Pandas dataframe
 data = pd.read_csv('success_widget.csv', index_col = 0)
@@ -19,10 +20,10 @@ what_fr = data_fr["What's wrong"]
 
 #plot what's wrong
 
-what_en.value_counts().sort_index().plot.bar(x='Reason', y='Number of occurrences')
+what_en.value_counts().sort_index().plot.barh(x='Reason', y='Number of occurrences')
 plt.show()
 
-what_fr.value_counts().sort_index().plot.bar(x='Raisons', y="Nombre d'occurences")
+what_fr.value_counts().sort_index().plot.barh(x='Raisons', y="Nombre d'occurences")
 plt.show()
 
 
@@ -31,14 +32,22 @@ plt.show()
 word_list_en = data_en["Details"].tolist()
 word_list_en = [str(i) for i in word_list_en]
 all_words_en = ' '.join([str(elem) for elem in word_list_en])
-from nltk.tokenize import word_tokenize
-tokens_en = word_tokenize(all_words_en)
+
+#tokenize words
+tokenizer = nltk.RegexpTokenizer(r"\w+")
+tokens_en = tokenizer.tokenize(all_words_en)
 words_en = []
 for word in tokens_en:
         words_en.append(word.lower())
 
+#remove nation
+words_en = list(filter(('nan').__ne__, words_en))
+
+#remove stop words to get most frequent words
 nltk.download('stopwords')
 sw = nltk.corpus.stopwords.words('english')
+sw.append('covid')
+sw.append('19')
 words_ns_en = []
 for word in words_en:
         if word not in sw:
@@ -46,5 +55,36 @@ for word in words_en:
 
 from nltk import FreqDist
 fdist1 = FreqDist(words_ns_en)
-most_common_en = fdist1.most_common(30)
+most_common_en = fdist1.most_common(50)
 print(most_common_en)
+
+#WordCloud
+word_cloud_en = ' '.join(words_ns_en)
+
+wordcloud = WordCloud(max_font_size=40).generate(word_cloud_en)
+
+import matplotlib.pyplot as plt
+
+plt.imshow(wordcloud, interpolation='bilinear')
+
+plt.axis("off")
+
+plt.show()
+
+
+#look at bigrams
+from nltk.collocations import *
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+finder = BigramCollocationFinder.from_words(
+        words_en)
+
+finder.apply_freq_filter(3)
+print (finder.nbest(bigram_measures.likelihood_ratio, 10))
+
+#look at trigrams
+trigram_measures = nltk.collocations.TrigramAssocMeasures()
+finder = TrigramCollocationFinder.from_words(
+        words_en)
+
+finder.apply_freq_filter(3)
+print (finder.nbest(trigram_measures.likelihood_ratio, 20))
