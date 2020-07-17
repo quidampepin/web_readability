307821d4fa7fccc61a1dbd6ed69c00e0aa302bcb
quidampepin <pepin.david@gmail.com>
2020-07-17 08:06:31 -0400

    Adding French capacity for most frequent words and adjusting README file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+(Le fraçais suit l'anglais)
 # Web readability score
 
-This is an attempt at tweaking the basic Flesch-Kincaid readability score to reflect how we write for the Web. It was developed with pages on Canada.ca in mind.
+This is an experimental way of calculating a readability score specifically for the web.
+It was developed with pages on Canada.ca in mind.
 
 
 ## Basic Flesch-Kincaid score
@@ -9,54 +11,84 @@ Basic Flesch-Kincaid uses 2 variables to get a score:
 - average sentence length
 - average number of syllables per word
 
-One issue on the web is that it doesn't handle headings and bullet lists very well. If there isn't a period at the end of a heading or a bullet list, it considers it to be still the same sentence.
+Basic readability score doesn't handle headings and bullet lists very well. If there isn't a period at the end of a heading or a bullet item, it considers it to be still the same sentence.
 
 Concretely, it means that a page with a lot of bullet points gets a bad Flesch-Kincaid score, even if we know it's often a good thing to use bullet points.
 
-One other issue is that it's ususally a good idea to keep paragraphs short and use headings to "chunk up" the text. I thought it would be nice to take these variables into account.
+We also know it's usually a good idea to keep paragraphs short and use headings to separate the text in chunks.
 
 ## Adjusted web readability score
-With my limited Python knowledge, I built a script that:
+This script:
 - scrapes the HTML of a URL
-- adds a period at the end of each heading and each bullet point (so each of these is considered as a “sentence” for the purpose of readability score)
-- only keeps what’s in the “main” tag
+- adds a period at the end of each heading and each bullet point (so each of these is considered as a “sentence” for the purpose of the readability score)
+- only keeps what’s in the “main” tag of the HTML page
 - attributes “points” (on a max of 100) based on the following metrics:
   - **Flesch-Kincaid score:** grade level at 6 or under gives the full 60 points, a grade level 18 and over gives 0 points, and anything in between is prorated
   - **Number of words between headings:** 40 words or less per heading gives 20 points, 200 words or more per heading gives 0 point, and anything in between is prorated
   - **Number of words per paragraph:** 30 words or less per paragraph gives 20 points, 80 words or more per paragraph gives 0 point, and anything in between is prorated
 
-It also give a list of the 15 most frequent words (excluding stop words) - it could be useful for SEO purposes.
+The script also gives a list of the 15 most frequent words (excluding stop words). This could be useful for SEO purposes.
 
-This is quite arbitrary, obviously, and it may not work for every page, but results are interesting.
 
-## Sample of script output
-When launching a script (in Python 3), it asks for a URL, and spits out a report.
+## How to use the script
+- Install Python 3, and all needed libraries:
+  - Flask
+  - bs4 (BeautifulSoup)
+  - readability
+  - requests
+  - nltk
+- Run read_score.py in a Python intepreter - it launched a local server
+- Go to http://localhost:5000/read_score, and pass 2 parameters:
+  - the language of the page
+  - the URL of the page
 
-For example, https://www.canada.ca/en/services/benefits/education/student-aid/grants-loans/repay.html gives an initial readability score of grade 11.96.  
-The adjusted Flesch-Kincaid score is 7.91 (because of all the bullet points and headings).  
-Converted into points and factoring in the words between headings and number of words per paragraph, it gives a score of 82.85/100.
+For example, if you go http://localhost:5000/read_score?lang=en&url=https://www.canada.ca/en/services/benefits.html, you'll get the readability score and most frequent words for the Benefits theme page.
 
-Here's the output:
 
-Initial readability score: 11.96  
-Total points: 82.85 /100  
-Basic readabilty points: 50.45 /60 (Flesch-Kincaid readability score of: 7.91)  
-Heading points:  12.41 /20 (words per heading: 100.75)  
-Paragraph points:  20.00 /20 (words per paragraph: 23.71)  
-Number of words:  806  
-Most used words  
-('rate', 20)  
-('interest', 19)  
-('repayment', 17)  
-('time', 15)  
-('payments', 14)  
-('period', 11)  
-('loan', 9)  
-('make', 9)  
-('6', 9)  
-('month', 9)  
-('student', 8)  
-('one', 7)  
-('payment', 7)  
-('school', 7)  
-('non', 7)  
+*******
+
+# Score de lisibilité Web
+
+Il s'agit d'une méthode expérimentale de calcul d'un score de lisibilité spécifique au Web.
+
+Il a été développé pour les pages de Canada.ca.
+
+
+## Note de base de Flesch-Kincaid
+
+Le score Flesch-Kincaid de base utilise 2 variables pour obtenir un score :
+- la longueur moyenne des phrases
+- le nombre moyen de syllabes par mot
+
+La note de lisibilité de base ne gère pas très bien les en-têtes et les listes à puces sur le Web. S'il n'y a pas de point à la fin d'une en-tête ou d'une puce, il considère qu'il s'agit toujours de la même phrase.
+
+Concrètement, cela signifie qu'une page avec beaucoup de puces obtient un mauvais score Flesch-Kincaid, même si nous savons que c'est souvent une bonne chose d'utiliser des puces.
+
+Nous savons aussi que c'est souvent une bonne idée de garder les paragraphes courts et d'utiliser des en-têtes pour séparer le texte en sections.
+
+## Score de lisibilité web ajusté
+Le script:
+- va chercher le code HTML d'une URL
+- ajoute un point à la fin de chaque titre et de chaque puce (chacun d'eux est donc considéré comme une "phrase" aux fins de la note de lisibilité)
+- ne conserve que ce qui se trouve dans la balise "main" de la page HTML
+- attribue des "points" (sur un maximum de 100) en fonction des paramètres suivants :
+  - **Score Flesch-Kincaid :** un niveau de 6 ou moins donne 60 points, un niveau de 18 et plus donne 0 point, et un score entre les deux est calculé au prorata
+  - **Nombre de mots entre les en-têtes :** 40 mots ou moins entre les en-têtes donne 20 points, 200 mots ou plus par titre donne 0 point, et un nombre entre les deux est calculé au prorata
+  - **Nombre de mots par paragraphe :** 30 mots ou moins par paragraphe donne 20 points, 80 mots ou plus par paragraphe donne 0 point, et un nombre entre les deux est calculé au prorata
+
+Le script donne également une liste des 15 mots les plus fréquents (en excluant des mots très fréquents). Cela pourrait être utile pour le les techniques d'optimisation de recherche (SEO).
+
+
+## Comment utiliser le script
+- Installez Python 3, et toutes les bibliothèques nécessaires :
+  - Flask
+  - bs4 (BeautifulSoup)
+  - readability
+  - requests
+  - nltk
+- Exécutez read_score.py dans un interpréteur Python - cela lance un serveur local
+- Allez sur http://localhost:5000/read_score, et incluez 2 paramètres :
+  - la langue de la page
+  - l'URL de la page
+
+Par exemple, si vous allez sur http://localhost:5000/read_score?lang=fr&url=https://www.canada.ca/fr/services/prestations.html, vous obtiendrez le score de lisibilité et les mots les plus fréquents pour la page thématique des avantages.
--- read_score.py
+++ read_score.py
@@ -8,109 +8,214 @@ app = Flask(__name__)
 
 def suggest():
 
-	#get url
-    url = request.args.get('url')
+    #get language
+    lang = request.args.get('lang')
 
-    import pandas as pd
+    if lang == 'en':
 
-    #get the html from the URL
-    import requests
-    r = requests.get(url)
-    html = r.text
+	    #get url
+        url = request.args.get('url')
 
-    #get the html content as text - get content from the "main" tag
-    from bs4 import BeautifulSoup
-    original_soup = BeautifulSoup(html, features="lxml").find('main')
-    original_text = original_soup.get_text()
+        #get the html from the URL
+        import requests
+        r = requests.get(url)
+        html = r.text
 
-    #get initial readability total_score
-    from readability import Readability
-    r_o = Readability(original_text)
-    original_fk = r_o.flesch_kincaid()
+        #get the html content as text - get content from the "main" tag
+        from bs4 import BeautifulSoup
+        original_soup = BeautifulSoup(html, features="lxml").find('main')
+        original_text = original_soup.get_text()
 
-    #add periods after bullet points and headings so that the Flesch Kicaid score considers them as sentences
-    html1 = html.replace("</li>", ".</li>")
-    html2 = html1.replace("</h1>", ".</h1>")
-    html3 = html2.replace("</h2>", ".</h2>")
-    html4 = html3.replace("</h3>", ".</h3>")
-    html5 = html4.replace("</h4>", ".</h4>")
-    html6 = html5.replace("</h5>", ".</h5>")
-    html7 = html6.replace("</h6>", ".</h6>")
+        #get initial readability total_score
+        from readability import Readability
+        r_o = Readability(original_text)
+        original_fk = r_o.flesch_kincaid()
 
-    #get adjusted readability total_score
-    revised_soup = BeautifulSoup(html7, features="lxml").find('main')
-    revised_text = revised_soup.get_text()
+        #add periods after bullet points and headings so that the Flesch Kicaid score considers them as sentences
+        html1 = html.replace("</li>", ".</li>")
+        html2 = html1.replace("</h1>", ".</h1>")
+        html3 = html2.replace("</h2>", ".</h2>")
+        html4 = html3.replace("</h3>", ".</h3>")
+        html5 = html4.replace("</h4>", ".</h4>")
+        html6 = html5.replace("</h5>", ".</h5>")
+        html7 = html6.replace("</h6>", ".</h6>")
 
-    from readability import Readability
-    r_f = Readability(revised_text)
-    final_fk = r_f.flesch_kincaid()
+        #get adjusted readability total_score
+        revised_soup = BeautifulSoup(html7, features="lxml").find('main')
+        revised_text = revised_soup.get_text()
+
+        from readability import Readability
+        r_f = Readability(revised_text)
+        final_fk = r_f.flesch_kincaid()
 
 
-    #tokenize the text for processing
-    import nltk
-    from nltk.tokenize import RegexpTokenizer
-    tokenizer = RegexpTokenizer('\w+')
-    tokens = tokenizer.tokenize(revised_text)
-    words = []
-    for word in tokens:
-        words.append(word.lower())
+        #tokenize the text for processing
+        import nltk
+        from nltk.tokenize import RegexpTokenizer
+        tokenizer = RegexpTokenizer('\w+')
+        tokens = tokenizer.tokenize(revised_text)
+        words = []
+        for word in tokens:
+            words.append(word.lower())
 
-    #remove stop words from the tokens to get only the meaningful words
-    nltk.download('stopwords')
-    sw = nltk.corpus.stopwords.words('english')
-    words_ns = []
-    for word in words:
-        if word not in sw:
-            words_ns.append(word)
+        #remove stop words from the tokens to get only the meaningful words
+        nltk.download('stopwords')
+        sw = nltk.corpus.stopwords.words('english')
+        words_ns = []
+        for word in words:
+            if word not in sw:
+                words_ns.append(word)
 
-    #get the 15 most used words in the text
-    from nltk import FreqDist
-    fdist1 = FreqDist(words_ns)
-    most_common = fdist1.most_common(20)
+        #get the 15 most used words in the text
+        from nltk import FreqDist
+        fdist1 = FreqDist(words_ns)
+        most_common = fdist1.most_common(20)
 
-    #get all headings and calculate how many words on average between headings
-    headings = original_soup.findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
-    hratio = len(words)/(len(headings))
+        #get all headings and calculate how many words on average between headings
+        headings = original_soup.findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+        hratio = len(words)/(len(headings))
 
-    #get all paragraphs and all bulleted list, and calculate how many words per paragraph on average
-    paragraphs = original_soup.findAll(['p', 'ul'])
-    pratio = (len(words)/len(paragraphs))
+        #get all paragraphs and all bulleted list, and calculate how many words per paragraph on average
+        paragraphs = original_soup.findAll(['p', 'ul'])
+        pratio = (len(words)/len(paragraphs))
 
-    #calculate points for readability
-    if final_fk.score <= 6:
-        fkpoints = 60
-    elif final_fk.score >= 18:
-        fkpoints = 0
-    else :
-        fkpoints = (60-((final_fk.score-6)*5))
+        #calculate points for readability
+        if final_fk.score <= 6:
+            fkpoints = 60
+        elif final_fk.score >= 18:
+            fkpoints = 0
+        else :
+            fkpoints = (60-((final_fk.score-6)*5))
 
-    #calculate points for number of words between headings
-    if hratio <= 40:
-        hpoints = 20
-    elif hratio >= 200:
-        hpoints = 0
-    else :
-        hpoints = (20-((hratio-40)*0.125    ))
+        #calculate points for number of words between headings
+        if hratio <= 40:
+            hpoints = 20
+        elif hratio >= 200:
+            hpoints = 0
+        else :
+            hpoints = (20-((hratio-40)*0.125    ))
 
-    #calculate points for number of words per paragraph
-    if pratio <= 30:
-        ppoints = 20
-    elif pratio >= 80:
-        ppoints = 0
-    else :
-        ppoints = (20-((pratio-30)*0.4))
+        #calculate points for number of words per paragraph
+        if pratio <= 30:
+            ppoints = 20
+        elif pratio >= 80:
+            ppoints = 0
+        else :
+            ppoints = (20-((pratio-30)*0.4))
 
-    #add all points
-    total_score = fkpoints+hpoints+ppoints
-    total_score = format(total_score, '.2f')
-    fkpoints = format(fkpoints, '.2f')
-    final_fk_score = format(final_fk.score, '.2f')
-    hpoints = format(hpoints, '.2f')
-    hratio = format(hratio, '.2f')
-    ppoints = format(ppoints, '.2f')
-    pratio = format(pratio, '.2f')
-    total_words = len(words)
+        #add all points
+        total_score = fkpoints+hpoints+ppoints
+        total_score = format(total_score, '.2f')
+        fkpoints = format(fkpoints, '.2f')
+        final_fk_score = format(final_fk.score, '.2f')
+        hpoints = format(hpoints, '.2f')
+        hratio = format(hratio, '.2f')
+        ppoints = format(ppoints, '.2f')
+        pratio = format(pratio, '.2f')
+        total_words = len(words)
 
+    if lang == 'fr':
+
+	    #get url
+        url = request.args.get('url')
+
+        #get the html from the URL
+        import requests
+        r = requests.get(url)
+        html = r.text
+
+        #get the html content as text - get content from the "main" tag
+        from bs4 import BeautifulSoup
+        original_soup = BeautifulSoup(html, features="lxml").find('main')
+        original_text = original_soup.get_text()
+
+        #get initial readability total_score
+        from readability import Readability
+        r_o = Readability(original_text)
+        original_fk = r_o.flesch_kincaid()
+
+        #add periods after bullet points and headings so that the Flesch Kicaid score considers them as sentences
+        html1 = html.replace("</li>", ".</li>")
+        html2 = html1.replace("</h1>", ".</h1>")
+        html3 = html2.replace("</h2>", ".</h2>")
+        html4 = html3.replace("</h3>", ".</h3>")
+        html5 = html4.replace("</h4>", ".</h4>")
+        html6 = html5.replace("</h5>", ".</h5>")
+        html7 = html6.replace("</h6>", ".</h6>")
+
+        #get adjusted readability total_score
+        revised_soup = BeautifulSoup(html7, features="lxml").find('main')
+        revised_text = revised_soup.get_text()
+
+        from readability import Readability
+        r_f = Readability(revised_text)
+        final_fk = r_f.flesch_kincaid()
+
+
+        #tokenize the text for processing
+        import nltk
+        from nltk.tokenize import RegexpTokenizer
+        tokenizer = RegexpTokenizer('\w+')
+        tokens = tokenizer.tokenize(revised_text)
+        words = []
+        for word in tokens:
+            words.append(word.lower())
+
+        #remove stop words from the tokens to get only the meaningful words
+        nltk.download('stopwords')
+        sw = nltk.corpus.stopwords.words('french')
+        words_ns = []
+        for word in words:
+            if word not in sw:
+                words_ns.append(word)
+
+        #get the 15 most used words in the text
+        from nltk import FreqDist
+        fdist1 = FreqDist(words_ns)
+        most_common = fdist1.most_common(20)
+
+        #get all headings and calculate how many words on average between headings
+        headings = original_soup.findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+        hratio = len(words)/(len(headings))
+
+        #get all paragraphs and all bulleted list, and calculate how many words per paragraph on average
+        paragraphs = original_soup.findAll(['p', 'ul'])
+        pratio = (len(words)/len(paragraphs))
+
+        #calculate points for readability
+        if final_fk.score <= 6:
+            fkpoints = 60
+        elif final_fk.score >= 18:
+            fkpoints = 0
+        else :
+            fkpoints = (60-((final_fk.score-6)*5))
+
+        #calculate points for number of words between headings
+        if hratio <= 40:
+            hpoints = 20
+        elif hratio >= 200:
+            hpoints = 0
+        else :
+            hpoints = (20-((hratio-40)*0.125    ))
+
+        #calculate points for number of words per paragraph
+        if pratio <= 30:
+            ppoints = 20
+        elif pratio >= 80:
+            ppoints = 0
+        else :
+            ppoints = (20-((pratio-30)*0.4))
+
+        #add all points
+        total_score = fkpoints+hpoints+ppoints
+        total_score = format(total_score, '.2f')
+        fkpoints = format(fkpoints, '.2f')
+        final_fk_score = format(final_fk.score, '.2f')
+        hpoints = format(hpoints, '.2f')
+        hratio = format(hratio, '.2f')
+        ppoints = format(ppoints, '.2f')
+        pratio = format(pratio, '.2f')
+        total_words = len(words)
 
     return render_template("read_score.html", total_score = total_score, fkpoints = fkpoints, final_fk_score = final_fk_score, hpoints = hpoints, hratio = hratio, ppoints = ppoints, pratio = pratio, total_words = total_words, most_common = most_common)
 
